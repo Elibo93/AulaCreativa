8ac77ffa476b4109140e20feaa715c467fbba9a0
Julian <elisabeth93.bo@gmail.com>
2025-12-02 18:38:15 +0000

    m

--- src/main/java/es/etg/daw/dawes/java/web/aulaCreativa/aulaCreativa/domain/repository/InscripcionRepository.java
+++ src/main/java/es/etg/daw/dawes/java/web/aulaCreativa/aulaCreativa/domain/repository/InscripcionRepository.java
@@ -3,10 +3,10 @@ package es.etg.daw.dawes.java.web.aulaCreativa.aulaCreativa.domain.repository;
 import java.util.List;
 import java.util.Optional;
 
+import es.etg.daw.dawes.java.web.aulaCreativa.aulaCreativa.domain.model.alumno.AlumnoId;
 import es.etg.daw.dawes.java.web.aulaCreativa.aulaCreativa.domain.model.inscripcion.Inscripcion;
 import es.etg.daw.dawes.java.web.aulaCreativa.aulaCreativa.domain.model.inscripcion.InscripcionId;
 import es.etg.daw.dawes.java.web.aulaCreativa.aulaCreativa.domain.model.taller.TallerId;
-import es.etg.daw.dawes.java.web.aulaCreativa.aulaCreativa.domain.model.alumno.AlumnoId;
 import es.etg.daw.dawes.java.web.aulaCreativa.common.domain.repository.CRUDRepository;
 
 public interface InscripcionRepository extends CRUDRepository<Inscripcion, InscripcionId> {
@@ -16,6 +16,6 @@ public interface InscripcionRepository extends CRUDRepository<Inscripcion, Inscr
     List<Inscripcion> getByTallerId(TallerId tallerId);
     
     Optional<Inscripcion> getByAlumnoAndTaller(AlumnoId alumnoId, TallerId tallerId);
-    
+
     boolean existsByAlumnoAndTaller(AlumnoId alumnoId, TallerId tallerId);
 }
